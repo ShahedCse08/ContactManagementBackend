3239266e29a1c4ad707e024b1afc152ae11c5f9d
Shahed Ahmed <74583341+ShahedCse08@users.noreply.github.com>
2024-11-09 16:23:07 +0600

    feeds

--- Datavances.Assessment/obj/Debug/net6.0/Datavanced.Assessment.AssemblyInfo.cs
+++ Datavances.Assessment/obj/Debug/net6.0/Datavanced.Assessment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Datavanced.Assessment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c916093b64cf0530afc16400dce4aedc2d58f4ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Datavanced.Assessment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Datavanced.Assessment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Persistence/obj/Debug/net6.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net6.0/Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c916093b64cf0530afc16400dce4aedc2d58f4ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
